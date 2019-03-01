20faf4df30563bd0eb574b7f855e69b1d2cce251
meenal kewat <happykraftsinnovations@gmail.com>
2019-03-01 15:48:29 +0530

    Image Scroller Using UICollectionView

---
+++ ImageSlider/CustomCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomCollectionViewCell.swift
+//  ImageSlider
+//
+//  Created by Meenal Kewat on 3/1/19.
+//  Copyright © 2019 Happykrafts Innovations. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var img: UIImageView!
+    
+}
--- ImageSlider/ViewController.swift
+++ ImageSlider/ViewController.swift
@@ -8,13 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var actressImage = [UIImage(named: "Natalie Portman"),UIImage(named: "Scarlett Johansson"),UIImage(named: "jennifer"),UIImage(named: "Emma Stone"),UIImage(named: "c"),UIImage(named: "Alexandra Daddario"),UIImage(named: "Natalie Portman"),UIImage(named: "Scarlett Johansson"),UIImage(named: "jennifer"),UIImage(named: "Emma Stone"),UIImage(named: "c"),UIImage(named: "Alexandra Daddario"),UIImage(named: "Natalie Portman"),UIImage(named: "Scarlett Johansson"),UIImage(named: "jennifer"),UIImage(named: "Emma Stone"),UIImage(named: "c"),UIImage(named: "Alexandra Daddario"),UIImage(named: "Natalie Portman"),UIImage(named: "Scarlett Johansson"),UIImage(named: "jennifer"),UIImage(named: "Emma Stone"),UIImage(named: "c"),UIImage(named: "Alexandra Daddario")]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+
+}
+
+//writing extension increse the readability
+extension ViewController: UICollectionViewDelegate,UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return actressImage.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCollectionViewCell
+        cell.img.image = actressImage[indexPath.row]
+        return cell
+        
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout{
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+     //   let size = UIScreen.main.bounds
+        let size = collectionView.frame.size
+        return CGSize(width: size.width, height: size.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 0
+    }
 
 }
 
